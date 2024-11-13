c504b7f40f09372b081b3a19c59ca99c8f5c6818
Max922991 <max43223@gmail.com>
2024-11-13 14:18:49 +0300

    add new files

--- src/main/resources/db/changelog/changesets/V1__init.sql
+++ src/main/resources/db/changelog/changesets/V1__init.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS wallet
 (
     wallet_id     UUID   NOT NULL PRIMARY KEY,
     balance       BIGINT NOT NULL,
-    transaction_Id UUID UNIQUE
+    transaction_id UUID UNIQUE
 );
 
 
